d731fe01ae1b140872c17274a6a031b23feaf0a9
akhsunna <akhsunna4@gmail.com>
2015-01-29 17:10:35 +0200

    add users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def show
-
+    @users = User.all
+    @u = User.find(params[:id])
   end
 
   def welcome
-    redirect_to current_user if user_signed_in?
+    # redirect_to current_user if user_signed_in?
   end
 end
