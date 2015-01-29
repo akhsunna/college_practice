8a77569ed095479b9a313fbed69153b7a9bfd8c7
akhsunna <akhsunna4@gmail.com>
2015-01-29 12:03:10 +0200

    create events_users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129094053) do
+ActiveRecord::Schema.define(version: 20150129095226) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",           limit: 255
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20150129094053) do
     t.integer  "members_number", limit: 4
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+  end
+
+  create_table "events_users", id: false, force: :cascade do |t|
+    t.integer "user_id",  limit: 4, null: false
+    t.integer "event_id", limit: 4, null: false
   end
 
   create_table "users", force: :cascade do |t|
