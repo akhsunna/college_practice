c17211d1de98202f79a5da14e42395fdb7956d46
Anna <akhsunna4@gmail.com>
2015-02-02 12:26:02 +0200

    fix user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :login, presence: true
+  validates :name, presence: true
 
   has_many :events_users, dependent: :delete_all
   has_many :events, through: :events_users
