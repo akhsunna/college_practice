51130e3c8ee51dc53f070294023091f86a14f70f
akhsunna <akhsunna4@gmail.com>
2015-02-12 11:43:30 +0200

    change comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :commentable, :polymorphic => true
-  has_many :comments, :as => :commentable
+  belongs_to :commentable, polymorphic: :true
+  has_many :comments, as: :commentable, dependent: :destroy
   belongs_to :user
 
   # def article
